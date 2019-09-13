a52cb872a0f85b60cffe1fc9c974b6c5b0c9779d
Loewy Malkovich <loewymalkov@gmail.com>
2019-09-13 14:19:33 -0700

    add select form layout for display by function

--- src/doctor-finder-interface.js
+++ src/doctor-finder-interface.js
@@ -5,17 +5,19 @@ import { SearchConditions } from './finder.js';
 
 $(document).ready(function() {
   $('#search').click(function() {
-    const location = $('#location').val();
+    const city = $('#location').val();
     $('#location').val("");
     const condition = $('#condition').val();
     $('#condition').val("");
+    const displayBy = $('#display-by').val();
+    $('#display-by').val();
 
     let searchConditions = new SearchConditions();
     let promise = searchConditions.getConditions(conditions);
   
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $('#show-doctor').text(`${location}, ${response.}`);
+      $('#show-doctor').text(`${city}, ${response.}`);
     }
   });
 });
--- src/finder.js
+++ src/finder.js
@@ -1,8 +1,8 @@
 export class SearchConditions {
-  getConditions(conditions) {
+  getConditions(location) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/`${process.env.apiKey}
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${location}&user_location=${location}&sort=${displayBy}&skip=0&limit=10&user_key=${process.env.apiKey}`
 
       request.onload = function() {
         if (this.status === 200) {
