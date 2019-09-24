1cabbe1ef632a83f918297e74955a4b6ddc9f9a5
Loewy Malkovich <loewymalkov@gmail.com>
2019-09-23 22:13:00 -0700

    add phone info for api return, change to single api call for symptom and name query, make results display fresh each time

--- src/doctor-finder-interface.js
+++ src/doctor-finder-interface.js
@@ -7,27 +7,23 @@ import { Search } from './search.js';
 $(document).ready(function() {
   $('#search').submit(function(event) {
     event.preventDefault();
-    const location = $('#location').val();
-    $('#location').val("");
     const symptom = $('#symptom').val();
     $('#symptom').val("");
     const name = $('#name').val();
     $('#name').val("");
     const displayBy = $('#display-by').val();
-    console.log('display by value', displayBy)
-    $('#display-by').val("");
 
-    let searchBySymptom = new Search();
-    let searchByName = new Search();
-    let promiseSymptom = searchBySymptom.getSymptom(symptom, displayBy);
-    let promiseName = searchByName.getName(name, displayBy);
+    let search = new Search();
     
-    promiseSymptom.then(function(response) {
+    let promise = search.getSymptom(symptom, name,displayBy);
+   
+    
+    promise.then(function(response) {
       const { data } = JSON.parse(response);
       for (let i=0; i < data.length; i++) {
         console.log('data[i]', data[i]);
         if (data[i].practices[0].name && data[i].profile.last_name) {
-          $('#show-doctor').append(`<li>${data[i].practices[0].name}, ${data[i].profile.last_name}</li>`)
+          $('#show-doctor').append(`<li>${data[i].practices[0].name}, ${data[i].profile.last_name}, ${data[i].practices[0].phones[0].number}</li>`)
         } else {
           'data incomplete';
         }
@@ -35,19 +31,6 @@ $(document).ready(function() {
     }, function(error) {
       $('#error').text(`${error.message}`);
     });
-
-    promiseName.then(function(response) {
-      const { data } = JSON.parse(response);
-      for (let i=0; i < data.length; i++) {
-        console.log('data[i]', data[i]);
-        if (data[i].practices[0].name && data[i].profile.last_name) {
-          $('#show-doctor').append(`<li>${data[i].practices[0].name}, ${data[i].profile.last_name} ${data[i].profile.first_name}</li>`)
-        } else {
-          'data incomplete';
-        }
-      }   
-    }, function(error) {
-      $('#error').text(`${error.message}`);
-    });
+    $('#show-doctor').text("");
   });
 });
--- src/search.js
+++ src/search.js
@@ -1,8 +1,8 @@
 export class Search {
-  getSymptom(symptom, displayBy) {
+  getSymptom(symptom, name, displayBy) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const urlSymptom = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=${displayBy}&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=${displayBy}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
@@ -12,24 +12,7 @@ export class Search {
           reject(Error(request.statusText));
         }
       }
-      request.open("GET", urlSymptom, true);
-      request.send();
-    });
-  }
-  getName(name, displayBy) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const urlName = `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=${displayBy}&skip=0&limit=10&user_key=${process.env.apiKey}`;
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", urlName, true);
+      request.open("GET", url, true);
       request.send();
     });
   }
